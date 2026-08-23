d2c9ee2173c81c634592054923d29b18dee4b631
Tolulopealamuoye <preciousalamuoye@gmail.com>
2026-08-23 13:32:57 +0200

    updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,15 +4,6 @@ resource "azurerm_resource_group" "rg" {
 }
 
 module "network" {
-<<<<<<< HEAD
-    source = "./modules/network"
-    resource_group_name = var.resource_group_name
-    location = var.location
-    application_name = var.application_name
-    environment = var.environment
-    vnet_address_space = var.vnet_address_space
-    public_subnet_address_prefixes = var.public_subnet_address_prefixes
-=======
   prefix                         = "book"
   source                         = "./modules/network"
   resource_group_name            = azurerm_resource_group.rg.name
@@ -21,33 +12,10 @@ module "network" {
   environment                    = var.environment
   vnet_address_space             = var.vnet_address_space
   public_subnet_address_prefixes = var.public_subnet_address_prefixes
->>>>>>> b9390c3 (updated)
 
 }
 
 module "compute" {
-<<<<<<< HEAD
-    source = "./modules/compute"
-    resource_group_name = var.resource_group_name
-    location = var.location
-    admin_username = var.adminuser
-    admin_password = var.admin_password
-    vnet_subnet_id = modules.network.public_subnet_id
-    application_name = var.application_name
-    environment = var.environment
-    vm_size = var.vm_size
-    ssh_public_key = var.ssh_public_key
-}
-
-module "database" {
-    source = "./modules/database"
-    resource_group_name = var.resource_group_name
-    location = var.location
-    mysql_admin_username = var.mysql_admin_username
-    mysql_admin_password = var.mysql_admin_password
-    mysql_database_name  = var.mysql_database_name
-    backend_vm_public_ip = module.compute.backend_public_ip
-=======
   prefix              = "book"
   source              = "./modules/compute"
   resource_group_name = azurerm_resource_group.rg.name
@@ -70,6 +38,5 @@ module "database" {
   mysql_admin_password = var.mysql_admin_password
   mysql_database_name  = var.mysql_database_name
   backend_vm_public_ip = module.compute.backend_public_ip
->>>>>>> b9390c3 (updated)
 
 }
